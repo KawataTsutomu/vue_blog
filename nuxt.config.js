e134172f964aba261091b76fb13d0ef31c006b99
kawatatsutomu <tomtom.net1@gmail.com>
2022-01-24 23:55:25 +0900

    vuetifyダークテーマを外す

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/prism',
+    '~/plugins/prism',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -63,7 +63,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
